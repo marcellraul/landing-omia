1735e39d5c0f990a1182cfae0ba575c7219ff4c9
Luis Matamala <lmatamala@digevo.com>
2021-01-13 13:50:06 -0300

    se agrega validación hash url

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LandingComponent } from './landing/landing.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: ':hash',
+    component: LandingComponent 
+  }
+];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forChild(routes)
   ]
 })
 export class ComponentsModule { }
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -6,14 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
-  public date: any;
-  public datehour: any;
-  constructor() {
-    this.date = new Date();
+  //public date: any;
+  //public datehour: any;
+
+  public hash;
+  public isValid: boolean = true;
+
+  constructor(private ruta: ActivatedRoute) {
+    /* this.date = new Date();
     this.date.getFullYear();
     this.datehour = new Date();
-    this.datehour.getFullYear();
+    this.datehour.getFullYear() */;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.ruta.snapshot.params);
+
+    this.hash = this.ruta.snapshot.params.hash;
+    
+    if(this.hash && this.hash === '0cda1f2904dc14e34ec69f5b23e36a0eb0fc222785ba496c41dba4a38bd9f9a5'){
+      this.isValid = true;
+    }else{
+      this.isValid = false;
+    }
+  }
 }
